4162b4afed939b243aad81f22c9df70f4268cb5b
chihiro <74747035+nishi019@users.noreply.github.com>
2021-04-14 17:43:53 -0700

    Timesheet page & a few update

--- timesheet.js
+++ timesheet.js
@@ -1,9 +1,9 @@
 //user = {firstname, lastname, profilePhoto} but 'User (index)' for this example 
 const userData = [['Employee User 1', 'images/user-icon.png'],
-                  ['Employee User 2', 'images/user-icon.png'],
-                  ['Employee User 3', 'images/user-icon.png'],
-                  ['Employee User 4', 'images/user-icon.png'],
-                  ['Employee User 5', 'images/user-icon.png'],
+                  ['Employee User 2', 'images/man1.png'],
+                  ['Employee User 3', 'images/calender-icon.png'],
+                  ['Employee User 4', 'images/timesheet-icon.png'],
+                  ['Employee User 5', 'images/key-icon.png'],
                   ['Employee User 6', 'images/user-icon.png'],
                   ['Employee User 7', 'images/user-icon.png'],
                   ['Employee User 8', 'images/user-icon.png'],
@@ -18,9 +18,15 @@ const userData = [['Employee User 1', 'images/user-icon.png'],
                   ['Employee User 17', 'images/user-icon.png'],
                   ['Employee User 18', 'images/user-icon.png'],
                   ['Employee User 19', 'images/user-icon.png'],];
+const user1 = [''];
+const totalTime = [8, '', 9, 12, '', 5];
 
 const monthInString = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 const dayInString = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+const timeInString = [' AM', ' PM'];
+const leftTh = ['Date', 'Day', 'Total'];
+const timeSeparation = ['00', '15', '30', '45'];
+
 const date = new Date();
 let year = date.getFullYear();
 let month = date.getMonth() + 1;
@@ -93,7 +99,7 @@ function createAllTimesheet(year, month) {
     let dayCount = 1;
     let timesheetHtml = '';
 
-    timesheetHtml += '<thead> <tr>' + '<th id="user-size">Employees</th>'
+    timesheetHtml += '<div class="table-contents"> <table> <thead> <tr>' + '<th id="user-head">Employees</th>'
 
     for(let d=0; d<endDayCount; d++){
         timesheetHtml  += '<th id="day-size">' +  dayCount + '</th>'
@@ -117,30 +123,79 @@ function createAllTimesheet(year, month) {
         }  
         timesheetHtml += '<td>150 hrs</td>'+ '</tr>'
     }
+    timesheetHtml += '</table></div>'
+
     return timesheetHtml;
 }
 
 
 function createIndividualUserTimesheet(i, year, month) {
-    //const startDate = new Date(year, month-1, 1);
+    const startDate = new Date(year, month-1, 1);
     const endDate = new Date(year, month, 0);
     const endDayCount = endDate.getDate();
+    const day = startDate.getDay();
+    // for col span 
+    var num_time = 4;
 
     let dayCount = 1;
     let timesheetHtml = '';
-
-    timesheetHtml += '<thead> <tr>' 
-                  + '<th id="user-size">'+ userData[i-1][0] +'</th>'
-
-    for(let d=0; d<endDayCount; d++){
-        timesheetHtml  += '<th id="day-size">' +  dayCount + '</th>'
-        dayCount++;
+    timesheetHtml += '<div id="table2" class="table2-contents"><table><thead>' 
+    for (let i=0; i<2; i++) {
+        timesheetHtml += '<tr>'
+        //1st row
+        if(i==0) {
+            timesheetHtml += '<th colspan="3" id="left-size"></th>'
+            for(let k=0; k<24; k++) {
+                timesheetHtml += '<th colspan="4" id="time-size">'
+                if(k===0) {
+                    timesheetHtml += (k+12)+timeInString[0]+'</th>';
+                } else if(k<12) {
+                    timesheetHtml += k+timeInString[0]+'</th>';
+                } else if(k===12) {
+                    timesheetHtml += k+timeInString[1]+'</th>';
+                } else {
+                    timesheetHtml += (k-12)+timeInString[1]+'</th>';
+                }    
+            }
+        }  else {
+            for(let l=0; l<3; l++) {
+                timesheetHtml += '<td id="sub-left-size">'+ leftTh[l]+'</td>'
+            }
+            for(let m=0; m<96; m++) {
+                timesheetHtml += '<td id="min-size">'+ timeSeparation[m%4]+'</td>'
+            }
+        } 
+        timesheetHtml += '</tr>'
     }
-    timesheetHtml += '<th id="hour-size"></th> </tr> </thread>'
+    timesheetHtml += '</thead>'
+    
+    for(let i=0; i<endDayCount; i++) {
+        var cur = i;
+        timesheetHtml += '<tr><td id="sub-left-height">'+month+"/"+(i+1)+'</td>'
+                      +'<td id="sub-left-height">'+dayInString[(day+i)%7]+'</td>'
+                      +'<td id="sub-left-height">'+displayTotalTime(totalTime[i%6])+'</td>'
+        for(let j=0; j<96; j++) {
+            if(totalTime[i%6]!=0 && cur==i){
+                var c_span = totalTime[i%6]*4;
+                timesheetHtml+='<td id="time-bar" colspan="'+c_span+'">'+totalTime[i%6]+' hour</td>'
+                cur++;    
+            }
+            timesheetHtml+= '<td></td>'
+        } 
+    }
 
+    timesheetHtml += '</table></div>'
 
     return timesheetHtml ;
 }
-
+function displayTotalTime(n){
+    if(n==0){
+        return ''
+    }else if(n<10){
+        return '0'+n+':00'
+    }else{
+        return n+':00'
+    }
+}
 showOption();
 showTimesheet(cur, year, month);
