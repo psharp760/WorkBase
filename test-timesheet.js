8a7ebf641611995f0f87554056edc5946004a779
chihiro <74747035+nishi019@users.noreply.github.com>
2021-05-12 16:05:41 -0700

    update test-timesheet

--- test-timesheet.js
+++ test-timesheet.js
@@ -1,43 +1,31 @@
 
-const monthInString = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-
 const date = new Date();
 let year = date.getFullYear();
 let month = date.getMonth() + 1;
 let cur = 0;
 
-function showTimesheet(i, year, month) {
-    document.querySelector('.month-box').innerHTML = monthInString[month-1] + '  ' + year;
-    let timesheetHtml = ''
-    if(i==0){
-        timesheetHtml = createAllTimesheet(year, month);
-    } else {
-        timesheetHtml = createIndividualUserTimesheet(i, year, month);
-    }
-    document.querySelector('#timesheet').innerHTML = timesheetHtml;
-    document.querySelector('#previous').addEventListener('click', moveTimesheet);
-    document.querySelector('#next').addEventListener('click', moveTimesheet);
-}
-
-
-
 function chooseTimesheet(v){
     //document.querySelector('.month-box').innerHTML = monthInString[month-1] + '  ' + year;
-
     var xml = new XMLHttpRequest();
     xml.onreadystatechange = function(){
         if(this.readState==4 && this.status==200){
             document.getElementById("timesheet").innerHTML = this.responseText;
         }
     };
-    xml.open("GET", "timesheet.php?q="+v,true);
+    xml.open("GET", "timesheet.php?q="+v, true);
     xml.send();
 
-    document.querySelector('#timesheet').innerHTML = timesheetHtml;
-    document.querySelector('#previous').addEventListener('click', moveTimesheet);
-    document.querySelector('#next').addEventListener('click', moveTimesheet);
+    //document.querySelector('#timesheet').innerHTML = timesheetHtml;
+    //document.querySelector('#previous').addEventListener('click', moveTimesheet);
+    //document.querySelector('#next').addEventListener('click', moveTimesheet);
 }
-
+$(document).ready(function(){
+    $("button").onclick(function(){
+        $.post("timesheet.php", function(){
+            alert("success!");
+        });
+    });
+});
 function moveTimesheet(e) {
     if(e.target.id === 'previous') {
         month--;
@@ -53,83 +41,20 @@ function moveTimesheet(e) {
             month = 1;
         }
     }
-    var m = document.getElementById("monthYear");
-    m.value = month.toString() + year.toString();
+    $.post("timesheet.php", {month: month, year: year},
+    function(data){
+        alert( "success");
+        $('#monthYear').html(data);
+    });
+
+    //var m = document.getElementById("monthYear");
+    //m.value = month.toString() + year.toString();
     //var v = document.getElementById("my-select").value;
     //chooseTimesheet(v);
     //document.querySelector('.month-box').innerHTML = monthInString[month-1] + '  ' + year;
 }
 
-function createAllTimesheet(year, month) {
-    //const startDate = new Date(year, month-1, 1);
-    const endDate = new Date(year, month, 0);
-    const endDayCount = endDate.getDate();
-  
-    let dayCount = 1;
-    let timesheetHtml = '';
 
-    timesheetHtml+='<div class="table-contents"><table><thead><tr>'+'<th id="user-head">Employees</th>'
-
-    for(let d=0; d<endDayCount; d++){
-        timesheetHtml+='<th id="day-size">'+dayCount+'</th>'
-        dayCount++;
-    }
-    timesheetHtml+='<th id="hour-size">Total</th></tr></thread>'
-    var list = makeAllList();
-    var all_list = new Array();
-    all_list.push(list);
-
-    for(let i=0; i<userData.length; i++) {
-        timesheetHtml+='<tr><td class="card"> <img src="'+ userData[i].user_img
-                     +'"> <span>' + userData[i].emp_name + '</span></td>'
-        var r_total = 0;
-        if(all_list.length>0){ // there is data in the all_list
-            var cur_id = all_list[0][0].e_id;
-            var cur_m = all_list[0][0].month; 
-            var cur_indv;
-            if(cur_id==i+1 && month==cur_m){ //if there is database for (user id# i) employee
-                cur_indv = all_list.shift();
-                for(let j=0; j<dayCount-1; j++){
-                    if(cur_indv.length>0){
-                        var cur_indv_date = cur_indv[0].date;
-                        if(cur_indv_date==(j+1)){
-                            if(i%2==0){
-                                timesheetHtml += '<td id=evenCell>'+cur_indv[0].s_time+' - '+cur_indv[0].e_time+'</td>'
-                            }else{
-                                timesheetHtml += '<td id=oddCell>'+cur_indv[0].s_time+' - '+cur_indv[0].e_time+'</td>'
-                            }
-                            r_total += cur_indv[0].total_min;
-                            cur_indv.shift();
-                        }else{
-                            timesheetHtml += '<td></td>'
-                        }
-                    }else{
-                        timesheetHtml += '<td></td>'
-                    }
-                } 
-            }else{
-                for(let j=0; j<dayCount-1; j++){
-                    timesheetHtml += '<td></td>'
-                }  
-            }
-        }else{ //no database for # i of user id
-            for(let j=0; j<dayCount-1; j++){
-                timesheetHtml += '<td></td>'
-            } 
-        }
-        var total_hour;
-        if(r_total==0){
-            total_hour = '00:00';
-        }else{
-            total_hour = parseInt(r_total/60)+':'+r_total%60;
-        }
-        timesheetHtml+='<td>'+total_hour+'</td>'+'</tr>' 
-    }
-    timesheetHtml+='</table></div>'
-
-    return timesheetHtml;
-}
-
-showOption();
-showTimesheet(cur, year, month);
-chooseTimesheet("");
+//showOption();
+//showTimesheet(cur, year, month);
+//chooseTimesheet("");
--- test-timesheet.php
+++ test-timesheet.php
@@ -1,126 +1,29 @@
 <?php 
+session_start();
+
 $year = date("Y");
 $month = date("m");
-session_start();
-$endDayCount = cal_days_in_month(CAL_GREGORIAN, $month, $year);
-$startDay = date('w', strtotime('Y-m-01'));
-$timeHour = array("4 AM","5 AM","6 AM","7 AM","8 AM","9 AM","10 AM","11 AM","12 PM","1 PM","2 PM","3 PM","4 PM","5 PM","6 PM","7 PM","8 PM","9 PM","10 PM","11 PM","12 AM","1 AM","2 AM","3 AM");
-$leftTh = array("Date", "Day", "Total");
-$timeSeparation = array("00", "15", "30", "45");
-$dayInString = array("Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat");
-$monthInString = array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
-$calendorText = $monthInString[$month-1]." ".$year;
 
-function getCaledarMonth($y, $m, $date){
-   $curYear = $y;
-   $curMonth = $m;
-   $unixMonth = mktime(0,0,0,$curMonth,1,$curYear);
-   $endDay = date('t',$unixMonth);
-   $prev = date('Y-m', strtotime('-1 month'), $unixMonth);
-   $next = date('Y-m', strtotime('+1 month'), $unixMonth);
-
-   $output = $curMonth." ".$curYear;
-
-   echo $output;
-}
 function getMonthYear($month, $year){
    $monthInString = array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
    $display .= "<div class='monthly-btns'></div>"
-            . "<button id='previous' type='submit' class='previousButton' name='previous' onchange='moveTimesheet'></button>"
+            . "<button id='previous' type='submit' class='previousButton' name='previous' onclick='moveTimesheet(this.id)'></button>"
             . "<div class='month-box' id='monthYear'>".$monthInString[$month-1]." ".$year."</div>"
-            . "<button id='next' type='submit' class='nextButton' name='next' onchange='moveTimesheet'></button>"
+            . "<button id='next' type='submit' class='nextButton' name='next' onclick='moveTimesheet(this.id)'></button>"
             . "</div></form>";
    echo $display;
 }
-
-include 'db_connection.php';
-$con = OpenCon();
-?>
-
-<!DOCTYPE html>
-<html>
-<head>
-   <link rel="stylesheet" href="style.css">
-   <meta charset="utf-8"/>
-   <script type="text/javascript" src="timesheet.js"></script>
-</head>
-<body>
-   <header>
-      <img  src="images/logo.png" alt="logo" id="logo_img">
-
-      <div class="nav-container">
-         <ul id="nav">
-            <li><img src="images/calendar-icon.png" id="icon-nav"><a href="calendar.php">Calendar</a></li>
-            <li><img src="images/timesheet-icon.png" id="icon-nav"><a href="timesheet.php" id="current">Timesheet</a></li>
-            <li><img src="images/user-icon.png" id="icon-nav"><a href="profile.php">Profile</a></li>
-            <li><img src="images/logout-icon.png" id="icon-nav"><a href="login.php">Logout</a></li>
-         </ul>
-      </div>
-   </header>
-<main> 
-<form action="timesheet.php" method="post">
-<div class="monthly-container">
-   <p class="select-option"> Employee: 
-      <select id="my-select" name="employee-options" onchange="chooseTimesheet(this.value)">
-         <option value="0" selected>all</option>
-         <option value="1">option1</option>
-         <option value="2">option2</option>
-         <?php  
-         //$sql = "SELECT first_name, last_name FROM users";
-         //$result = mysqli_query($con, $sql);
-         //while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
-               //echo "<option value ='".$row['user_id']."'>".$row['first_name']." ".$row['last_name']."</option>"; 
-         //}
-         ?>
-      </select>
-   </p>
+function IndividualTimesheet($month, $year, $q){
+   $timeHour = array("4 AM","5 AM","6 AM","7 AM","8 AM","9 AM","10 AM","11 AM","12 PM","1 PM","2 PM","3 PM","4 PM","5 PM","6 PM","7 PM","8 PM","9 PM","10 PM","11 PM","12 AM","1 AM","2 AM","3 AM");
+   $leftTh = array("Date", "Day", "Total");
+   $timeSeparation = array("00", "15", "30", "45");
+   $dayInString = array("Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"); 
+   $endDayCount = cal_days_in_month(CAL_GREGORIAN, $month, $year);
+   $startDay = date('w', strtotime($year.'-'.$month.'-1'));
+   $display = "";
+   global $con; 
    
-<div id="timesheet" name="ts"></div>
-<?php
-//--------------- Calendar Movement ----------------------------
-$display = "";
-if(isset($_POST['ts'])){
-   
-}else{
-   
-}
-
-if(isset($_POST['previous'])){
-   $month--;
-   if($month<1){
-      $year--;
-      $month=12;
-   }
-   getMonthYear($month, $year);
-}else if(isset($_POST['next'])){
-   $month++;
-   if($month>12){
-      $year++;
-      $month=1;
-   }
-   getMonthYear($month, $year);
-}else{
-   $date = date("Y-m-d");
-   $year = date("Y");
-   $month = date("m");
-   getMonthYear($month, $year);
-}
-
-//----------------- Timesheet contents -----------------------------------------
-if(isset($_GET['q']))
-{
-   $q = intval($_GET['q']);
-
-//$sql = "SELECT user_id, first_name, last_name FROM users ORDER BY user_id";
-//$result = mysqli_query($con, $sql);
-//$resultCount = mysqli_num_rows($result);
-
-//$display = "";
-
-// Timesheet contents (all or individual timesheet)
-if($q>0){ // Individual timesheet
    $display .= "<div id='table2' class='table2-contents'><table><thead>";
-
    for($i=0; $i<2; $i++){
       $display .= "<tr>";
       if($i===0){
@@ -140,57 +43,62 @@ if($q>0){ // Individual timesheet
    }
    $display .= "</thead>";
 
-   mysqli_select_db($con, "timesheet");
-   $sql = "SELECT DAY(start_time) AS sDay, 
-                  FORMAT(start_time, 'HH') AS sHour, 
-                  FORMAT(start_time, 'mm') AS sMin, 
-                  DATEDIFF(minute, start_time, end_time) AS workMin
-           FROM timesheet WHERE id = '".$q."' AND start_time = '".$year."-".$month."' 
-           ORDER BY start_time";
-   $res = mysqli_query($con, $sql);
-   $resCount = mysqli_num_rows($res);
+   //mysqli_select_db($con, "timesheet");
+   //$sql = "SELECT DAY(start_time) AS sDay, 
+                  //FORMAT(start_time, 'HH') AS sHour, 
+                  //FORMAT(start_time, 'mm') AS sMin, 
+                  //DATEDIFF(minute, start_time, end_time) AS workMin
+           //FROM timesheet WHERE id = '".$q."' AND start_time = '".$year."-".$month."' 
+           //ORDER BY start_time";
+   //$res = mysqli_query($con, $sql);
+   //$resCount = mysqli_num_rows($res);
 
-   for($i=0; $i<$endDayCount+1; $i++){
+   for($i=0; $i<$endDayCount; $i++){
       $display .= "<tr><td id='sub-left-height'>".$month."/".($i+1)."</td> 
-            <td id='sub-left-height'>".$dayInString[($startDay+$i+2)%7]."</td>";
-      if($resCount>0){ // Data in the database
-         while($row = mysqli_fetch_array($res)){
-            if(($i+1)===intval($row['sDay'])){
-               $workMin = intval($row['workMin']);
-               $wHour = floor($workMin/60);
-               $wMin = $workMin%60;
-               $display .= "<td id='sub-left-height'>".$wHour.":".$wMin."</td>";
+            <td id='sub-left-height'>".$dayInString[($startDay+$i)%7]."</td>";
+      //if($resCount>0){ // Data in the database
+         //while($row = mysqli_fetch_array($res)){
+            //if(($i+1)===intval($row['sDay'])){
+               //$workMin = intval($row['workMin']);
+               //$wHour = floor($workMin/60);
+               //$wMin = $workMin%60;
+               //$display .= "<td id='sub-left-height'>".$wHour.":".$wMin."</td>";
                
-               $span = round($workMin/15);
-               $sInMin = floor(intval($row['sHour'])/60) + intval($row['sMin'])%60;
-               for($j=0; $j<96; $j++){
-                  if((j+16)===$sInMin){
-                     if($wMin<1){
-                        $display .= "<td id='time-bar' colspan='".$span."'>".$wHour." hour</td>";
-                     }else{
-                        $display .= "<td id='time-bar' colspan='".$span."'>".$wHour." hour '".$wMin." min</td>";
-                     }
-                     $j+=$span;
-                  }else{
-                     $display .= "<td></td>";
-                  }
-               }
-            }else{
-               for($j=0; $j<96; $j++){
-                  $display .= "<td></td>";
-               }
-            }
-         }
-      }else{ // No data
-         for($j=0; $j<96; $j++){
+               //$span = round($workMin/15);
+               //$sInMin = floor(intval($row['sHour'])/60) + intval($row['sMin'])%60;
+               //for($j=0; $j<96; $j++){
+                  //if((j+16)===$sInMin){
+                     //if($wMin<1){
+                        //$display .= "<td id='time-bar' colspan='".$span."'>".$wHour." hour</td>";
+                     //}else{
+                        //$display .= "<td id='time-bar' colspan='".$span."'>".$wHour." hour '".$wMin." min</td>";
+                     //}
+                     //$j+=$span;
+                  //}else{
+                     //$display .= "<td></td>";
+                  //}
+               //}
+            //}else{
+               //for($j=0; $j<96; $j++){
+                  //$display .= "<td></td>";
+               //}
+            //}
+         //}
+      //}else{ // No data
+         for($j=0; $j<97; $j++){
             $display .= "<td></td>";
          }
-      }
+      //}
    }
    $display .= "</table></div>";
+
    echo $display;
+}
+function AllTimesheet($month, $year){
+   global $con; 
+   $display = "";
+   $endDayCount = cal_days_in_month(CAL_GREGORIAN, $month, $year);
 
-}else{ // All timesheet
    $display .= "<div class='table-contents'> <table> <thead> <tr> 
          <th id='user-head'>Employees</th>";
 
@@ -199,6 +107,7 @@ if($q>0){ // Individual timesheet
    }
    $display .= "<th id='hour-size'>Total</th></tr></thread>";
    $count=0;
+
    if($resCount>0){
       while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
          $display .= "<tr><td class='card'> <img src='".$row['user_img']."'><span>".$row['first_name']." ".$row['last_name']."</span></td>";
@@ -257,80 +166,108 @@ if($q>0){ // Individual timesheet
       }
    }
    $display .= "</table></div>";
-   echo $display; 
-} // end of Individual timesheet contents.
-}else{
-   $display .= "<div class='table-contents'> <table> <thead> <tr> 
-         <th id='user-head'>Employees</th>";
 
-   for($i=1; $i<$endDayCount+1; $i++){
-      $display .= "<th id='day-size'>".$i."</th>";
-   }
-   $display .= "<th id='hour-size'>Total</th></tr></thread>";
-   $count=0;
-   if($resCount>0){
-      while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
-         $display .= "<tr><td class='card'> <img src='".$row['user_img']."'><span>".$row['first_name']." ".$row['last_name']."</span></td>";
-         $curId = $row['user_id'];
-         $isql = "SELECT DAY(start_time) AS sDay, 
-                         FORMAT(start_time, 'HH-mm') AS start_hour_min,  
-                         FORMAT(end_time, 'HH-mm') AS end_hour_min, 
-                         DATEDIFF(minute, start_time, end_time) AS workMin 
-                  FROM timesheet WHERE id = '".$curId."' AND start_time = '".$year."-".$month."'";
-         $ires = mysqli_query($con, $isql);
-         $iresCount = mysqli_num_rows($ires);
-         $total=0;
-         if($iresCount>0){
-            while($irow = mysqli_fetch_array($ires, MYSQLI_ASSOC)){
-               for($j=0; $j<$endDayCount+1; $j++){
-                  if(intval($irow['sDay'])===($j+1)){
-                     if($count%2===0){
-                        $display .= "<td id=evenCell>".$irow['start_hour_min']." - ".$irow['end_hour_min']."</td>";
-                     }else{
-                        $display .= "<td id=oddCell>".$irow['start_hour_min']." - ".$irow['end_hour_min']."</td>";
-                     }
-                     $total+=intval($irow['workMin']);
-                  }else{
-                     $display .= "<td></td>";
-                  }
-               }  
-            }
-         }else{
-            for($j=0; $j<$endDayCount+1; $j++){
-               $display .= "<td></td>";
-            }
-         }
-         if($total===0){
-            $total_display = "00:00"; 
-         }else{
-            $total_display = floor($total/60).":".($total%60);
-         }
-         $display .= "<td>".$total_display."</td></tr>";  
-      }
-      if($resCount<11){
-         for($k=0; $k<(11-$resCount); $k++){
-            $display .= "<tr><td class='card'><span></span></td>";
-            for($j=0; $j<$endDayCount+1; $j++){
-               $display .= "<td></td>";
-            }
-            $display .= "<td></td></tr>"; 
-         }
-      }
-   }else{
-      for($k=0; $k<11; $k++){
-         $display .= "<tr><td class='card'><span></span></td>";
-            for($j=0; $j<$endDayCount+1; $j++){
-               $display .= "<td></td>";
-            }
-            $display .= "<td></td></tr>"; 
-      }
-   }
-   $display .= "</table></div>";
    echo $display; 
 }
 
+include 'db_connection.php';
+$con = OpenCon();
+?>
 
-  
+<!DOCTYPE html>
+<html>
+<head>
+   <link rel="stylesheet" href="style.css">
+   <meta charset="utf-8"/>
+   <script type="text/javascript" src="timesheet.js"></script>
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+</head>
+<body>
+   <header>
+      <img  src="images/logo.png" alt="logo" id="logo_img">
+
+      <div class="nav-container">
+         <ul id="nav">
+            <li><img src="images/calendar-icon.png" id="icon-nav"><a href="calendar.php">Calendar</a></li>
+            <li><img src="images/timesheet-icon.png" id="icon-nav"><a href="timesheet.php" id="current">Timesheet</a></li>
+            <li><img src="images/user-icon.png" id="icon-nav"><a href="profile.php">Profile</a></li>
+            <li><img src="images/logout-icon.png" id="icon-nav"><a href="login.php">Logout</a></li>
+         </ul>
+      </div>
+   </header>
+<main> 
+<form action="timesheet.php" method="post">
+<div class="monthly-container">
+   <p class="select-option"> Employee: 
+      <select id="my-select" name="employee-options" onchange="chooseTimesheet(this.id)">
+         
+         <option value="1">option1</option>
+         <?php 
+         //<option value="0">all</option>
+
+         //$sql = "SELECT first_name, last_name FROM users";
+         //$result = mysqli_query($con, $sql);
+         //while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
+               //echo "<option value ='".$row['user_id']."'>".$row['first_name']." ".$row['last_name']."</option>"; 
+         //}
+         ?>
+      </select>
+   </p>
+   
+<div id="timesheet"></div>
+<?php
+//--------------- Calendar Movement ----------------------------
+$display = "";
+//if(isset($_SERVER['HTTP_CACHE_CONTROL']) && $_SERVER['HTTP_CACHE_CONTROL']==='max-age=0'){
+   //unset($_SESSION);
+//}
+
+if(!isset($_SESSION['month'])){
+   $year = date("Y");
+   $month = date("m");
+   $_SESSION['month'] = $month;
+   $_SESSION['year'] = $year;
+}
+if(isset($_POST['previous'])){
+   $year = intval($_POST['year']);
+   //$month = intval($_POST['month']);
+   $month = $_SESSION['month'];
+   $year = $_SESSION['year'];
+   $month--;
+   if($month<1){
+      $year--;
+      $month=12;
+   }
+   $_SESSION['month'] = $month;
+   $_SESSION['year'] = $year;
+   getMonthYear($month, $year);
+}else if(isset($_POST['next'])){
+   $month = $_SESSION['month'];
+   $year = $_SESSION['year'];
+   $month++;
+   if($month>12){
+      $year++;
+      $month=1;
+   }
+   $_SESSION['month'] = $month;
+   $_SESSION['year'] = $year;
+   getMonthYear($month, $year);
+}else{
+   $month = $_SESSION['month'];
+   $year = $_SESSION['year'];
+   getMonthYear($month, $year);
+}
+
+
+//----------------- Timesheet contents -----------------------------------------
+//if(isset($_GET['q']))
+$option = intval($_POST['employee-options']);
+$q = intval($_GET['q']);
+if($option>0){ 
+   IndividualTimesheet($_SESSION['month'], $_SESSION['year'], $q);
+}else{
+   AllTimesheet($_SESSION['month'], $_SESSION['year']);
+}
 Close($con);
 ?>
 </main>
